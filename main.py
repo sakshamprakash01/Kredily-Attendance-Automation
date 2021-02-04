039a0c9cf58b7eb13a4e8df3ad940675fa474f4e
Ubuntu <ubuntu@ip-172-31-93-17.ec2.internal>
2021-02-04 17:25:24 +0530

    Changes for multiple persons

--- main.py
+++ main.py
@@ -5,23 +5,29 @@ import time
 from pprint import pprint
 
 def main():
-
+    sec = 1800
     data = json.load(open('credentials.json'))
 
-    clockInTime = data["ClockIn"]
-    clockOutTime = data["ClockOut"]
-    clockDays = data["ClockDays"]
-
-    for day in clockDays:
-        exec("schedule.every()."+day+".at(clockInTime).do(clock.clockIn)")
-        exec("schedule.every()."+day+".at(clockOutTime).do(clock.clockOut)")
+    for person in data:
+        clockInTime = person["ClockIn"]
+        clockOutTime = person["ClockOut"]
+        clockDays = person["ClockDays"]
+        for day in clockDays:
+            exec("schedule.every()."+day+".at(clockInTime).do(clock.clockIn, person)")
+            exec("schedule.every()."+day+".at(clockOutTime).do(clock.clockOut, person)")
+        
+    pprint(schedule.jobs)
+    print("Total People = ",len(data))
+    print("Total Jobs = ",len(schedule.jobs))
+    print("Frequency = "+str(sec)+"s")
+    print("SCRIPT RUNNING")
 
     while True:
 
         schedule.run_pending()
-        pprint(schedule.jobs)
-        print()
-        time.sleep(60)
+        # pprint(schedule.jobs)
+        # print()
+        time.sleep(sec)
                     
 if __name__ == "__main__":
     main()
